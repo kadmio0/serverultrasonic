1f6d06be0342173be6931022166b0d6ce579c009
kevin camacho <kadmio2@gmail.com>
2020-03-26 14:14:14 -0400

    fix distance value

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
-    res.send("Distance = "+sensor.distanceValue);
+    res.send("Distancia = "+sensor.distanceValue);
 });
 
 app.post('/',function(req,res){
